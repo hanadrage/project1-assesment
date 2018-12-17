cf60ce8b2f4854c227cad011db75ef0edc7fe481
hanadrage <43888774+hanadrage@users.noreply.github.com>
2018-12-17 13:24:26 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ document.addEventListener("DOMContentLoaded", function(e){
 	var number = 0;
 	var increment;
 	var decrement;
+	var heading1;
+	var heading2;
 	var numberValue = document.getElementById("inp")
 	document.getElementById('negative').addEventListener('click', function(){
 		number += 1;
 		var increment = document.getElementById("nn");
 		increment.value = number;
+		var heading1 = document.getElementById("inp");
+		heading1.innerHTML = number;
 	});
 	document.getElementById('plus').addEventListener('click', function(){
 		number -= 1;
 		var decrement = document.getElementById("nn");
 		decrement.value = number;
+		var heading2 = document.getElementById("inp");
+		heading2.innerHTML = number;
 	});	
 })
 
